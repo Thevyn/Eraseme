fe9d05050175b6abdcd08b18e6162007608c269b
Thevy Ngo <thevyngo.1998@gmail.com>
2020-03-31 23:19:24 +0200

    Update imports

--- src/main/java/esito/controller/EraseController.java
+++ src/main/java/esito/controller/EraseController.java
@@ -1,18 +1,12 @@
 package esito.controller;
 
-import esito.Connect;
-
 import esito.model.Customer;
 import esito.repository.CustomerRepository;
 import esito.repository.EraseRepository;
-import no.esito.anonymizer.ContextFactory;
-import no.esito.anonymizer.IContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Method;
-import java.sql.Connection;
 import java.util.List;
 
 @RestController
