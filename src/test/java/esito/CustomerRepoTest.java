914c3313d262acd2c905622f3c8ee2e67ac70839
Thevy Ngo <thevyngo.1998@gmail.com>
2020-03-31 22:13:49 +0200

    Unit tests

---
+++ src/test/java/esito/CustomerRepoTest.java
@@ -0,0 +1,33 @@
+package esito;
+
+import esito.model.Customer;
+import esito.repository.CustomerRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerRepoTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Test
+    public void eraseCustomer() {
+        customerRepository.eraseCustomer("1000234");
+        Customer customer = customerRepository.findCustomerById("1000234");
+
+        Assert.assertEquals("firstname lastname", customer.getName() );
+        Assert.assertEquals("email@email.com",customer.getEmail());
+    }
+
+    @Test
+    public void findUserById() {
+        Customer customer = customerRepository.findCustomerById("1000234");
+        Assert.assertNotNull(customer);
+    }
+}
