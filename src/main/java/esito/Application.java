002e27ab30171a7117290defdab7f7d8169b0c77
Thevy Ngo <thevyngo.1998@gmail.com>
2020-03-31 15:24:06 +0200

    Autowiring dataSource

--- src/main/java/esito/Application.java
+++ src/main/java/esito/Application.java
@@ -1,13 +1,25 @@
 package esito;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+    @Autowired
+    DataSource dataSource;
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Connection Polling datasource : "+ dataSource);  // check connection pooling
+    }
+
 
 }
