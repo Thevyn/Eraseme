04264020429b4e747279baa65147d8e116c8e388
Thevy Ngo <thevyngo.1998@gmail.com>
2020-03-31 22:13:56 +0200

    Create index.js

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,53 @@
+$( function() {
+    getAllCustomer();
+});
+
+
+function getAllCustomer() {
+    $.get( "/api/getCustomers", (data) => {
+        showCustomer(data);
+    });
+}
+
+function showCustomer(customer) {
+    // lag tabell
+
+    let ut =
+        "<table class='table '><tr>" +
+        "<th>CreditCard</th><th>CustomerNo</th><th>Name</th><th>Email</th>" +
+        "<th>Password</th><th>Phone</th>" +
+        "</tr>";
+    for (const c of customer) {
+        ut += "<tr>";
+        ut +=
+            "<td>" +
+            c.creditCard +
+            "</td><td>" +
+            c.customerNo +
+            "</td><td>" +
+            c.name +
+            "</td><td>" +
+            c.email +
+            "</td>" +
+            "<td>" +
+            c.password +
+            "</td><td>" +
+            c.phone +
+            "</td>";
+        ut += "</tr>";
+    }
+    ut += "</table>";
+
+    $("#customers").html(ut);
+}
+
+function eraseCustomer(e) {
+    e.preventDefault();
+
+    var customerNo = $("#customerNo").val();
+    $.post("/api/Erase_CUSTOMER?identifier="+customerNo);
+
+    $("#customerNo").val("")
+    getAllCustomer();
+
+}
